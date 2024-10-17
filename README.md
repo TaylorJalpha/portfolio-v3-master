1ea6b7e83292b0df75d157739175456454029a3f
TaylorJalpha <taylorjacobferguson@gmail.com>
2024-10-16 19:48:47 -0700

    ship it

---
+++ README.md
@@ -0,0 +1,44 @@
+# ⚡️nuxt-bento-portfolio
+
+## A personal portfolio website ported from Astro to `Nuxt`. Currently is Work in progress, I'm improving the code because I migrated the entire project to nuxt in free time.
+
+<img width="1320" alt="Screenshot 2023-12-15 alle 15 39 14" src="https://github.com/apdev95/bento-portfolio-nuxt/assets/73004253/37b120e1-6000-435d-bf8c-c65248f495c6">
+
+To view a demo example, **[click here](https://andreapuglisi.io/)**
+
+## Features
+
+- Modern and Minimal bento-like, sleek UI Design
+- All in one page (almost)
+- Fully Responsive
+
+## Tech Stack
+
+- [Nuxt/Vue 3](https://nuxt.com/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [motion](https://motion.dev/)
+
+# Steps ▶️
+
+```bash
+# Clone this repository
+$ git clone https://github.com/apdev95/bento-portfolio-nuxt.git
+```
+
+```bash
+# Go into the repository
+$ cd bento-portfolio-nuxt
+```
+
+```bash
+# Install dependencies
+$ npm install
+```
+
+```bash
+# Start the project in development
+$ npm run dev
+```
+## Authors ❤️
+
+- Ported to Nuxt by Me, but Thank you for your hard work [@Ladvace](https://www.github.com/Ladvace) aka Gianmarco - https://github.com/Ladvace | https://gianmarco.xyz/ 
---
+++ components/About.vue
@@ -0,0 +1,30 @@
+<template>
+    <Card colSpan="md:col-span-1" rowSpan="md:row-span-4">
+        <div class="flex flex-col gap-2">
+            <h2 class="text-xl font-bold m-0 z-20">About me</h2>
+            <p class="text-sm font-light">
+                Product Manager (PMC/ PLC) and web developer with over 8 years of total experience.
+                <br />
+
+            </p>
+            <p class="text-sm font-light">
+                Primary Toolbox:
+            </p>
+            <ul class="list-disc list-inside">
+                <li>Product Management </li>
+                <li>Product Discovery</li>
+                <li>Data Analysis </li>
+                <li>Ruby on Rails </li>
+                <li>HTML | CSS | JavaScript </li>
+                <li>Vue | NUXT (new) </li>
+                <li>Tailwind CSS | Bootstrap </li>
+            </ul>
+            <p class="text-sm font-light">
+                Product Management is my focus, with web development primarily being through consulting, side projects, or to support the teams I previously managed.
+            </p>
+            <p class="text-sm font-light">
+                Put starkly, I truly enjoy the process of building and shipping digital services. 
+            </p>
+        </div>
+    </Card>
+</template>
