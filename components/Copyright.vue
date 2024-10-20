2b394a1c9aec232b01765a4ff91251fcc4a1a4d1
TaylorJalpha <taylorjacobferguson@gmail.com>
2024-10-20 05:32:33 -0700

    copy update

--- components/Copyright.vue
+++ components/Copyright.vue
@@ -5,7 +5,7 @@ import moment from 'moment';
 <template>
     <Card colSpan="md:col-span-1" rowSpan="md:row-span-2">
         <p class="text-xs mt-3">
-            Fork of <a href="https://github.com/Ladvace/astro-bento-portfolio" target="_blank" class="text-red-500">Ladvace </a> Astro theme by <a href="https://github.com/apdev95/bento-portfolio-nuxt" target="_blank" class="text-red-500">apdev95</a>– Created with <a href="https://nuxt.com/" target="_blank" class="text-red-500">Nuxt.</a>
+            Fork of <a href="https://github.com/Ladvace/astro-bento-portfolio" target="_blank" class="text-red-500">Ladvace </a> Astro theme by <a href="https://github.com/apdev95/bento-portfolio-nuxt" target="_blank" class="text-red-500">apdev95</a>– Created with <a href="https://nuxt.com/" target="_blank" class="text-[#41B883]">Nuxt.</a>
             <br><br> <a href="https://www.linkedin.com/in/taylor-jacob-ferguson/" target="_blank" class="text-red-500">Taylor Ferguson </a>. © {{
                 moment().format('Y') }}
         </p>
