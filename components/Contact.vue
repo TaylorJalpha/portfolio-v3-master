7111750642459306f149db7fa1debc1b13fd2cdf
TaylorJalpha <taylorjacobferguson@gmail.com>
2024-12-16 21:35:49 -0800

    MIB

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,39 +1,32 @@
 <template>
-  <Card colSpan="md:col-span-1" rowSpan="md:row-span-4">
-    <div class="h-full">
-      <header class="flex items-center">
-        <h1 class="text-white text-xl font-bold">Let's sync up!</h1>
-      </header>
-      <div class="flex flex-col mt-4">
-        <h2 class="text-gray-500">Contact</h2>
-        <p>📧 taylorjacobferguson @gmail.com</p>
-        <p>📍Los Angeles,CA</p>
-      </div>
+    <Card colSpan="md:col-span-1" rowSpan="md:row-span-4">
+        <div class="h-full">
+            <header class="flex items-center">
+                <h1 class="text-white text-xl font-bold">Let's sync up!</h1>
+            </header>
+            <div class="flex flex-col mt-4">
+                <h2 class="text-gray-500">Contact</h2>
+                <p>📧 taylorjacobferguson @gmail.com</p>
+                <p>📍Los Angeles,CA</p>
+            </div>
 
-      <div class="flex flex-col mt-4 w-fit">
-        <h2 class="text-gray-500">Social Network</h2>
-        <ul>
-          <li>
-            <a href="https://instagram.com/tayloralphaa" target="_blank"
-              >Instagram</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/taylor-jacob-ferguson/"
-              target="_blank"
-              >LinkedIn</a
-            >
-          </li>
-          <li>
-            <a href="https://www.strava.com/athletes/63030031" target="_blank">Strava</a>
-          </li>
-          <li>
-            <a href="https://github.com/TaylorJalpha" target="_blank">GitHub</a>
-          </li>
-          
-        </ul>
-      </div>
-    </div>
-  </Card>
+            <div class="flex flex-col mt-4 w-fit">
+                <h2 class="text-gray-500">Social Network</h2>
+                <ul>
+                    <li>
+                        <a href="https://instagram.com/tayloralphaa" target="_blank" class="gradient font-light text-base hover:text-red-500">Instagram</a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/taylor-jacob-ferguson/" target="_blank" class="text-sm font-light hover:text-red-500">LinkedIn</a>
+                    </li>
+                    <li>
+                        <a href="https://www.strava.com/athletes/63030031" target="_blank" class="text-sm font-light  hover:text-red-500">Strava</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/TaylorJalpha" target="_blank" class="text-sm font-light  hover:text-red-500">GitHub</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </Card>
 </template>
