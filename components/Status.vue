80c6a53a4404c48fa333843cde509ad5c7fb9446
TaylorJalpha <taylorjacobferguson@gmail.com>
2025-01-09 16:46:54 -0800

    MIB

--- components/Status.vue
+++ components/Status.vue
@@ -1,17 +1,23 @@
-
-
 <template>
-    <Card :colSpan="'lg:col-span-2'" :rowSpan="'md:row-span-1'" title="Time zone">
-        <div class="flex justify-between w-full items-start mb-2">
-            <div class="flex flex-col text-xl">
-                <h2>Project Status</h2>
-            </div>
-            <span class="relative flex h-3 w-3">
-                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-            </span>
-        </div>
-        <p class="text-md">Currently working on a few initiatives..<a href="https://formability.eu" target="_blank"></a></p>
-    </Card>
-    
+  <Card :colSpan="'lg:col-span-2'" :rowSpan="'md:row-span-1'" title="Time zone">
+    <div class="flex justify-between w-full items-start mb-2">
+      <div class="flex flex-col text-xl">
+        <h2>Project Status</h2>
+      </div>
+      <span class="relative flex h-3 w-3">
+        <span
+          class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"
+        ></span>
+        <span
+          class="relative inline-flex rounded-full h-3 w-3 bg-green-500"
+        ></span>
+      </span>
+    </div>
+    <p class="text-md">
+      Currently working on a few initiatives..<a
+        href="https://formability.eu"
+        target="_blank"
+      ></a>
+    </p>
+  </Card>
 </template>
