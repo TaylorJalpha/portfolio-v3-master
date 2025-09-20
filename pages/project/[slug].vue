e87ebccd7734905aec84c07ed07fde0b27ee80f3
TaylorJalpha <taylorjacobferguson@gmail.com>
2025-09-20 06:52:59 -0700

    updaed portfolio item project, slug, view, QROQ query string update

--- pages/project/[slug].vue
+++ pages/project/[slug].vue
@@ -13,11 +13,7 @@ const portfolioItem = ref<any>(null)
 
 onMounted(async () => {
   const slug = Array.isArray(route.params.slug) ? route.params.slug[0] : route.params.slug
-  const query = `*[_type == "project" && (slug.current == "${slug}" || _id == "${slug}")][0]{
-    ...,
-    "content": body,
-    tags[]->{_id, title}
-  }`
+  const query = `*[_type in [\"project\", \"caseStudy\", \"blogPost\"] && (slug.current == \"${slug}\" || _id == \"${slug}\")][0]{ _id, title, description, slug, _type, featuredImage{ asset->{_id, url} }, tags[]->{ _id, title }, published_at, external_url, content, markdown, galleryImages, pdfFile{ asset->{url,_ref} } }`
   const data = await fetchSanityContent(query)
   console.log('Project [slug] fetchSanityContent response:', data)
   portfolioItem.value = { data }
