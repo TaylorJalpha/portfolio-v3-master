209a8303b6c8e8e02c867d3fc4baafecc2a0c40f
TaylorJalpha <taylorjacobferguson@gmail.com>
2025-09-20 15:43:31 -0700

    added dynamic meta tags per portfolio item type - project, case study, and blog

--- pages/project/[slug].vue
+++ pages/project/[slug].vue
@@ -5,17 +5,33 @@
 <script setup lang="ts">
 definePageMeta({ layout: 'portfolio-detail' })
 import { useRoute } from 'vue-router'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { fetchSanityContent } from '@/services/sanityApi'
+import { useHead } from '#imports'
 
 const route = useRoute()
 const portfolioItem = ref<any>(null)
 
 onMounted(async () => {
   const slug = Array.isArray(route.params.slug) ? route.params.slug[0] : route.params.slug
-  const query = `*[_type in [\"project\", \"caseStudy\", \"blogPost\"] && (slug.current == \"${slug}\" || _id == \"${slug}\")][0]{ _id, title, description, slug, _type, featuredImage{ asset->{_id, url} }, tags[]->{ _id, title }, published_at, external_url, content, markdown, galleryImages, pdfFile{ asset->{url,_ref} } }`
+  const query = `*[_type in [\"project\", \"caseStudy\", \"blogPost\"] && (slug.current == \"${slug}\" || _id == \"${slug}\")][0]{ _id, title, description, metadata, slug, _type, featuredImage{ asset->{_id, url} }, tags[]->{ _id, title }, published_at, external_url, content, markdown, galleryImages, pdfFile{ asset->{url,_ref} } }`
   const data = await fetchSanityContent(query)
   console.log('Project [slug] fetchSanityContent response:', data)
   portfolioItem.value = { data }
+})
+
+// Watch for portfolioItem and set meta tags when loaded
+watch(portfolioItem, (val) => {
+  if (val && val.data) {
+    useHead({
+      title: val.data.title || 'Project',
+      meta: [
+        {
+          name: 'description',
+          content: val.data.metadata || val.data.description || ''
+        }
+      ]
+    })
+  }
 })
 </script>
